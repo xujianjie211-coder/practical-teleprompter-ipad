018adfa659d14c95025d1de14013a6fc8ad28fc4
xujianjie211-coder <xujianjie211@gmail.com>
2026-08-12 20:53:06 +0800

    Fix UIApplicationDelegate window visibility

--- ipad/Sources/AppDelegate.swift
+++ ipad/Sources/AppDelegate.swift
@@ -2,7 +2,7 @@ import UIKit
 
 @main
 final class AppDelegate: UIResponder, UIApplicationDelegate {
-    private(set) var window: UIWindow?
+    var window: UIWindow?
 
     func application(
         _ application: UIApplication,
